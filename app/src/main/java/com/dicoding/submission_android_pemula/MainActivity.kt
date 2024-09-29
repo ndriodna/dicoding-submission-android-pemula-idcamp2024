e15adf083ad11abcd5e4fa7e10ef0d643eb3b948
ndriodna <kurodesu6969@gmail.com>
2024-09-29 23:12:53 +0800

    splashscreen & action menu

---
+++ app/src/main/java/com/dicoding/submission_android_pemula/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.dicoding.submission_android_pemula
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.dicoding.submission_android_pemula.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.title = "Wuthering Waves Waifu"
+
+
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.about_menu -> {}
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
---
+++ app/src/main/java/com/dicoding/submission_android_pemula/SplashScreenActivity.kt
@@ -0,0 +1,18 @@
+package com.dicoding.submission_android_pemula
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        val moveIntent = Intent(this,MainActivity::class.java)
+        startActivity(moveIntent)
+        finish()
+    }
+}
